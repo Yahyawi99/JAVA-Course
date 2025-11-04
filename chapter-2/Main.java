c3b21e87c2b6bd908fedde891544801c23e9b388
Yahyawi99 <yassinyahyawi26@gmail.com>
2025-11-04 21:50:22 +0100

    chapter-2

---
+++ chapter-2/Main.java
@@ -0,0 +1,18 @@
+
+// defaults :
+// integer number : int
+// decimal number : double
+
+public class Main {
+  public static void main(String[] args) {
+    String name = "T-Rex";
+    int age = 25;
+    String species = "Tyrannosaurus";
+    String diet = "carnivore";
+    float weight = 1000.50F;
+
+    System.out.println("Meet " + name + ", a " + age + "-year-old " + species + ". As a " + diet
+        + ", it has a robust weight of " + weight + " kilograms.");
+
+  }
+}
