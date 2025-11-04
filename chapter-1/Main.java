46492c1547630508ae74d2c93266219a1d8b3533
Yahyawi99 <yassinyahyawi26@gmail.com>
2025-11-04 20:50:18 +0100

    Java-course : chapter-1

---
+++ chapter-1/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+  public static void main(String args[]) {
+    String openingHours = "08:00";
+    String closingHours = "20:00";
+
+    System.out.println("Welcome to Mesozoic Eden!");
+    System.out.println("========================");
+    System.out.println("Opening hours: " + openingHours);
+    System.out.println("Closing hours: " + closingHours);
+    System.out.println("========================");
+    System.out.println("Mesozoic Eden is safe and secure.");
+  }
+}
