f01f10d5079f73de06af267030b2ba9cca20a004
Yahyawi99 <yassinyahyawi26@gmail.com>
2026-02-22 17:09:38 +0000

    chapter3

--- chapter-3/Main.java
+++ chapter-3/Main.java
@@ -1,4 +1,18 @@
 
 public class Main {
+  public static void main(String args[]) {
 
+    String name = "Tyrannosaurus";
+    float weight = 3500.6F;
+    int per = 6;
+
+    float foodWeight = (weight * per) / 100;
+
+    int numberOfFeeding = 3;
+    float singleFeedingWeight = foodWeight / numberOfFeeding;
+
+    System.out.println("Our " + weight + "kg " + name + " dinosaur needs to eat");
+    System.out.println(foodWeight + "kg daily, which means we need to serve " + singleFeedingWeight + "kg per feeding");
+
+  }
 }
